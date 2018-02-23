ad135f5cf2c98d9eb31d5e43c40b95281fa6a664
profyoni <jrobinson@touro.edu>
2018-02-22 21:20:30 -0500

    Update PaintApp.java

--- src/edu/touro/mco364/PaintApp.java
+++ src/edu/touro/mco364/PaintApp.java
@@ -2,9 +2,45 @@ package edu.touro.mco364;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.util.LinkedList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Pong extends JFrame implements ActionListener{
+    Pong()
+    {
+        setSize(500,500);
+        JButton button = new JButton("Press");
+        add(button, BorderLayout.NORTH);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+        button.addActionListener(this );
+    }
+
+    int x = 100, y= 200;
+    Timer timer ;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        timer = new Timer(100, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Graphics g = getGraphics();
+                x+=5;
+                y+=7;
+                g.fillOval(x,y,30,30);
+            }
+        });
+        timer.start();
+    }
+}
+
 
 public class PaintApp extends JFrame {
 
@@ -33,14 +69,79 @@ public class PaintApp extends JFrame {
         }
 
     }
+
+    private ExecutorService threadPool = Executors.newFixedThreadPool(100);
+
+    private StringBuffer buffer = new StringBuffer();
+    int z = 5;
+    javax.swing.Timer timer = new javax.swing.Timer(100,
+            new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    synchronized (buffer) {
+                        outputArea.setText("");
+                        outputArea.append(buffer.toString());
+                        buffer.delete(0, buffer.length());
+                    }
+//
+                }
+            });
+    JTextArea outputArea;
     public PaintApp() {
         setTitle("Amazing Paint App!");
 
         setSize(600, 800);
         JPanel canvas = new CanvasPanel();
 
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
         add(BorderLayout.CENTER, canvas);
 
+        JButton button = new JButton("Press Me");
+
+        outputArea = new JTextArea(100, 15);
+        timer.start();
+
+        JScrollPane scroller = new JScrollPane(outputArea);
+        add(BorderLayout.EAST, scroller);
+
+        add(BorderLayout.NORTH, button);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                threadPool.submit(new Runnable() {
+                                      @Override
+                                      public void run() {
+                                          for (int i = 0; i < 1000000; i++) {
+                                              String counter = Thread.currentThread().getName() + ":" + i;
+                                              System.out.println(counter);
+                                              System.out.println(z);
+
+                                              buffer.append(counter + '\n');
+
+//                                              SwingUtilities.invokeLater(
+//                                                      new Runnable() {
+//                                                          @Override
+//                                                          public void run() {
+//                                                              outputArea.append(counter + '\n');
+//                                                          }
+//                                                      }
+//                                              );
+
+                                              try {
+                                                  Thread.sleep(0);
+                                              } catch (InterruptedException e1) {
+                                                  e1.printStackTrace();
+                                              }
+                                          }
+                                      }
+                                  }
+                );
+            }
+        });
+        z++;
         JLabel statusBar = new JLabel("Status: ");
         add(BorderLayout.SOUTH, statusBar);
 
@@ -63,10 +164,13 @@ public class PaintApp extends JFrame {
             }
         });
 
-        setSize(800,600);
+        setSize(800, 600);
         setVisible(true);
 
+
+
     }
+
     LinkedList<Point> drawnHistory = new LinkedList<>();
     Point prev;
 }
